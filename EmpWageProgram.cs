a9f9a88e2bb403a9c66f79c7354cce8a1f865389
ameejac <ameejacherukuru123@gmail.com>
2021-09-10 21:33:27 -0700

    [Emp wage calculation]

--- EmpWageProgram.cs
+++ EmpWageProgram.cs
@@ -2,23 +2,34 @@ using System;
 
 namespace employeewages
 {
-    class us1
+
+    class EmpWageCal
     {
         static void Main(string[] args)
         {
             //Constants
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HUR = 20;
             Random random = new Random();
+            //variables
+            int empHrs = 0;
+            int empWage = 0;
             ///Computation
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("employee is present");
+                empHrs = 8;
+                Console.WriteLine("IS_FULL_TIME");
             }
             else
             {
-                Console.WriteLine("employee is abcent");
+                empHrs = 0;
+                Console.WriteLine("employeeWage ");
             }
+            empWage = empHrs * EMP_RATE_PER_HUR;
+            Console.WriteLine("empHrs Wage :" + empWage)
+            }
+
 	}
     }
 }
