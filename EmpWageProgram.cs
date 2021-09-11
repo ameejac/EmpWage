e22d88c36dcd582e109605a92da961aa1dd2063b
ameejac <ameejacherukuru123@gmail.com>
2021-09-10 22:15:21 -0700

    [emp-cal-for-mnth-uc5]

--- EmpWageProgram.cs
+++ EmpWageProgram.cs
@@ -4,34 +4,41 @@ namespace employeewages
 {
 
     class EmpWageCal
-    {
+   {
         static void Main(string[] args)
         {
-            //Constants
-            const  int IS_PART_TIME = 1;
+           //Constants
+            const int IS_PART_TIME = 1;
             const int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HUR = 20;
-
-            Random random = new Random();
-            //variables
-            int empHrs = 0;
-            int empWage = 0;
+            const int NUM_OF_WORKING_DAYS = 20;
+            const int EMP_RATE_PER_HUR = 20;
+                //variables
+                int empHrs = 0;
+                int empWage = 0;
+                int totalEmpWage = 0;
             ///Computation
-            int empCheck = random.Next(0, 3);
-            switch(empCheck)
+            for(int days = 0; days < NUM_OF_WORKING_DAYS; days++)
             {
-                case IS_PART_TIME:
-                   empHrs = 4;
-                  break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
-            //Console.WriteLine("employeeWage ");
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                        //Console.WriteLine("employeeWage ");
+                }
+                empWage = empHrs * EMP_RATE_PER_HUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("emp Wage : " + empWage);
             }
-            empWage = empHrs * EMP_RATE_PER_HUR;
-	  }  Console.WriteLine("emp Wage :" +empWage)
+            Console.WriteLine("total Emp Wage : " + totalEmpWage);
+        }  
     }
 }
