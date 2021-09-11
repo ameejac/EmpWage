e1e1fc32729d0e8803915514bc9a005dc379a6f1
ameejac <ameejacherukuru123@gmail.com>
2021-09-10 22:07:11 -0700

    [emp-switch-case-uc4]

--- EmpWageProgram.cs
+++ EmpWageProgram.cs
@@ -8,28 +8,30 @@ namespace employeewages
         static void Main(string[] args)
         {
             //Constants
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
+            const  int IS_PART_TIME = 1;
+            const int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HUR = 20;
-           // int PART_TIME_HRS = 8;
+
             Random random = new Random();
             //variables
             int empHrs = 0;
             int empWage = 0;
             ///Computation
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_PART_TIME)
+            int empCheck = random.Next(0, 3);
+            switch(empCheck)
             {
-                empHrs = 8;
-                Console.WriteLine("IS_PART_TIME");
-            }
-            else
-            {
-                empHrs = 0;
-                Console.WriteLine("employeeWage ");
+                case IS_PART_TIME:
+                   empHrs = 4;
+                  break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            //Console.WriteLine("employeeWage ");
             }
             empWage = empHrs * EMP_RATE_PER_HUR;
-            Console.WriteLine("empHrs Wage :" + empWage);
-	}
+	  }  Console.WriteLine("emp Wage :" +empWage)
     }
 }
